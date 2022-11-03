4bb3810762255ec99aaf0f04d16f27f332688f25
american111 <117360022+american111@users.noreply.github.com>
2022-11-03 17:35:06 +0500

    Add files via upload

---
+++ salohiddin.py
@@ -0,0 +1,81 @@
+# # class Odam:
+# #     def __init__ (albatta, ism, yosh):
+# #         albatta.ism=ism
+# #         albatta.yosh=yosh
+# #     def tanishtir(albatta):
+# #         print(f'{albatta.ism} ismim\n yoshim {albatta.yosh}')
+# # class Dasturchi(Odam):
+# #     def __init__(self,ism,yosh,tajriba):
+# #         super().__init__(ism,yosh)
+# #         self.tajriba=tajriba
+# #
+# #
+# # a=Dasturchi(ism='Odiljon',yosh=15,tajriba='ortacha')
+# # a.tanishtir()
+# # from uuid import uuid4
+# #
+# # a=uuid4()
+# #
+# # print(a)
+# # from uuid import uuid4
+# # class Avto:
+# #     def __init__(self,nomi,rangi):
+# #         self.nomi=nomi
+# #         self.rangi=rangi
+# #         self.probeg=0
+# #         self.id=uuid4()
+#
+#
+# m1 = Avto('Nexia','Oq')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
